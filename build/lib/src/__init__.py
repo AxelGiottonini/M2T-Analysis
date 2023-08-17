e3b015881cfcecf3a4103262e142ff2206f66b6f
Axel Giottonini <axel.giottonini@unifr.ch>
2023-08-17 16:46:09 +0200

    remove build

--- build/lib/src/__init__.py
+++
@@ -1,4 +0,0 @@
-from .ddmut import DDMut
-from .dynamut2 import DynaMut2
-from .maestro import Maestro
-from .utils import prune_dict, safe_open
--- build/lib/src/utils.py
+++
@@ -1,6 +0,0 @@
-
-def safe_open(f):
-    return open(f, "rb") if f is not None else None
-
-def prune_dict(d):
-    return {k:v for k, v in d.items() if v is not None}
